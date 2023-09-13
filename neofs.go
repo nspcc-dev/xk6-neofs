df3aa2d2e5f220c9203de20115756409205acaf1
Evgenii Baidakov <evgenii@nspcc.io>
2023-09-13 11:45:44 +0400

    xk6_neofs: Fix linter - blank-imports
    According to the repo this file is a main module. Thus, just left the comment next to the blank imports to justifying it.
    
    Signed-off-by: Evgenii Baidakov <evgenii@nspcc.io>

--- neofs.go
+++ neofs.go
@@ -1,9 +1,13 @@
 package xk6_neofs
 
 import (
+	// In fact, xk6_neofs is a main module, but with different name. Leave a comment here to solve linter warning.
 	_ "github.com/nspcc-dev/xk6-neofs/internal/datagen"
+	// In fact, xk6_neofs is a main module, but with different name. Leave a comment here to solve linter warning.
 	_ "github.com/nspcc-dev/xk6-neofs/internal/native"
+	// In fact, xk6_neofs is a main module, but with different name. Leave a comment here to solve linter warning.
 	_ "github.com/nspcc-dev/xk6-neofs/internal/registry"
+	// In fact, xk6_neofs is a main module, but with different name. Leave a comment here to solve linter warning.
 	_ "github.com/nspcc-dev/xk6-neofs/internal/s3"
 	"go.k6.io/k6/js/modules"
 )
